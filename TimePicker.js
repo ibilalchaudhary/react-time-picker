cc6ab7bc66db99f1f87cb65e43bdcd3a1dd28b90
ibilalchaudhary <bilalchaudhary478@gmail.com>
2021-12-22 14:39:57 +0500

    done

---
+++ TimePicker.js
@@ -0,0 +1,9 @@
+import React from "react";
+import TimePicker from "react-time-picker";
+
+export default function TimePicker() {
+  return (
+    <TimePicker />
+    //   onChange={setSlotStartTime} value={slotStartTime}
+  );
+}
